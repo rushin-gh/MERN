eccc72aff29b644d8df9dbbf17ccbb802fd17eb0
rushin-gh <rushin.mailme@gmail.com>
2025-06-26 00:03:52 +0530

    Learn JS

---
+++ JavaScript/Datatypes.js
@@ -0,0 +1,12 @@
+/*
+String
+Number
+Boolean
+Bigint
+
+undefined
+null
+
+Object -> Object, Array
+Symbol
+*/
---
+++ JavaScript/Printing.js
@@ -0,0 +1,7 @@
+console.log('Printining things!');
+
+process.stdout.write('process.stdout.writeline\n');
+
+console.table({age: 12});
+
+console.warn({age: 13});
